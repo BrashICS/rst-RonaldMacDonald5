2b454fdd6df0e0d201bc93e7fccd21584bda57b4
Matthew MacDonald <matthew.macdonald4@stu.ocsb.ca>
2024-06-04 09:51:11 -0400

    working

--- step_2/script.js
+++ step_2/script.js
@@ -1,3 +1,12 @@
 'use strict';
 
-
+let blackcats = 1170
+let kaws4 = 2379
+let monarch = 80
+let brb = 300
+let airmag = 37136
+let blackaf1 = 150
+let travis = 1839
+let mcq = 120
+let nbrb = 240
+let 
