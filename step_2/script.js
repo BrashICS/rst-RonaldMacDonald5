6c3b9b43ff41bcfa68282195475726e932b0026b
Matthew MacDonald <matthew.macdonald4@stu.ocsb.ca>
2024-06-17 08:28:50 -0400

    yurp

--- step_2/script.js
+++ step_2/script.js
@@ -46,4 +46,44 @@ function show_image(src, width, height, alt) {
 }
   console.log(show_image(kaws4.jpg, 200, 200,alt))
 
+const shoesData = [
+  { price:1170, image: 'blackcats.webp'},
+  { price:2379, image: 'kaws4.jpg'},
+  {price:80, image: 'monarch.jpg'},
+  {price:300, image:'bigredboots.jpg'},
+  {price:37136, image:'airmag.webp'},
+  {price:150,  image:'blackaf1'},
+  {price:1839, image:'travis.webp'},
+  {price:120,  image:'mc1.webp'},
+  {price:240,  image:'1906rnb.webp'},
+  {price:439,  image:'bred11.jpg'},
+  {price:221, image:'foamrunners.jpg'},
+  {price:9979,  image:'diorjordan.jpg'},
+  {price:230, image:'airmax97.png'},
+  {price:95,  image:'nikerunner.png'},
+  {price:2100000, image:'solidgold.avif'},
+  {price:135,  image:'ugg.jpeg'}
+];
+
+function updateShoeImage () {
+  const shoeindex = document.getElementById('shoeSelect').value;
+  const shoeImage = document.getElementById('shoeimage');
+  shoeImage.src = shoesData[shoeIndex].image;
+
+}
+
+function chechGuess () {
+  const shoeIndex = document.getElementById('shoeSelect').value
+  const userGuess = document.getElementById('guessInput').value
+  const guessNumber = parseInt(userGuess);
+  const actualPrice = shoesData [shoeIndex].price;
+
+  if (guessNumber < actualPrice) {
+    alert("Your guess is too low. Try a higher price");
+  } else if (guessNumber > actualPrice) {
+    alert("Your guess is too high. Try a lower price")
+  } else {
+    alert("Congratulations! You guessed the correct price");
+  }
+  }
 
